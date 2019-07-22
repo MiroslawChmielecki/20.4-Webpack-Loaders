5863ea402dc67f0e42dfe9ae2799e43812f1bd22
Miroslaw Chmielecki <misiak19@op.pl>
2019-07-22 14:20:51 +0200

    Changes in .babelrc

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,13 @@ const path = require('path');
 
 //webpack.config.js
 module.exports = {
+    
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'build'),
         filename: 'app.bundle.js'
     },
+    
     module: {
         rules: [
             {
