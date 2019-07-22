faefb87c0de8283989e5edf3de083cdf8da96e99
Miroslaw Chmielecki <misiak19@op.pl>
2019-07-22 14:34:06 +0200

    Add styles

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import style from './Title.css';
 
 
 const Title = props => 
-    <div>
+    <div className={style.TitleItem}>
         <h1>{props.title}</h1> 
         <p>Number: {props.taskNumber}</p>
     </div>
